b058b21b9c7faa2ac659da87d0faed12eaf5a3b7
Zoe Roux <zoe.roux@sdg.moe>
2021-11-02 10:29:19 +0100

    Creating a lambda unit test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,15 @@
+import Test.HUnit
+import System.Exit ( exitFailure, exitSuccess )
+
+test1 :: Test
+test1 = TestCase (assertEqual "for (foo 3)," (1,3) (1, 3))
+
+tests :: Test
+tests = TestList [TestLabel "test1" test1]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    counts <- runTestTT tests
+    if errors counts + failures counts == 0
+        then exitSuccess
+        else exitFailure
