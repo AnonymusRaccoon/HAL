7e2b003c20214e5f5bade2311417cb7b437497e8
Zoe Roux <zoe.roux@sdg.moe>
2021-11-03 10:54:45 +0100

    Adding a REPL

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
 module Main where
 
 import Lib
+import System.Console.Haskeline
+    ( defaultSettings, getInputLine, outputStrLn, runInputT, InputT )
+import BasicParser
+import LispParser
 
 main :: IO ()
-main = someFunc
+main = runInputT defaultSettings loop
+   where
+       loop :: InputT IO ()
+       loop = do
+           minput <- getInputLine " > "
+           case minput of
+               Nothing -> return ()
+               Just "exit" -> return ()
+               Just "quit" -> return ()
+               Just input -> (outputStrLn . show $ parse pLisp input) >> loop
